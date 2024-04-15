3d455583ad2649e33f8015401cb5e23c85f0aabb
smsmpanda <64564713+smsmpanda@users.noreply.github.com>
2024-04-16 07:32:41 +0800

    String

--- ks0415.c
+++ ks0415.c
@@ -14,10 +14,36 @@ void PrintBasicTypeSizeof() {
 	printf("\tdouble \t\tsizeof :%d\n", sizeof(double));
 }
 
+/// <summary>
+/// 字符串
+/// </summary>
+void StringType()
+{
+	//'\0'是字符串结束标志，不计入字符串长度
+	char str[] = "abc";
+	char str1[] = { 'a', 'b', 'c' };
+
+	printf("str len :%d\n", strlen(str));
+
+	//随机值，因为没有找到'\0'结束字符
+	printf("str1 len :%d\n", strlen(str1));
+
+	char* ss = "abcd";
+	printf("ss len is %d\n", strlen(ss));
+
+	// 转义字符
+	char str2[] = "c:\\aa\\na\\ta\n";
+	printf(str2);
+
+}
+
+
 int main() {
 	PrintBasicTypeSizeof();
 	extern int g_val;
 	printf("extern int g_val:%d", g_val);
+
+	StringType();
+
 	return 0;
-	
 }
